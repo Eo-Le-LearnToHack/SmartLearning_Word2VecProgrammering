3b3caee651e7cc688ab45c4694d27b15e021a890
Kristian Toustrup Langborg-Hansen <ktlh@smartlearning.dk>
2021-11-26 19:07:36 +0100

    Opdateret rækkefølge af ord i kaldet til webservicen

--- Word2VecProgrammering/Program.cs
+++ Word2VecProgrammering/Program.cs
@@ -5,8 +5,9 @@ string[] ord = new string[3];
 // Skriv koden her, som giver brugeren mulighed for at indtaste 3 ord,
 // som efterfølgende bliver gemt i array'et ord
 
+
 // Det er er min kode, som du ikke behøver at rette i
-string url = $"https://smartword2vec.azurewebsites.net/api/Top3Similar?positive1={ord[1]}&positive2={ord[3]}&negative={ord[2]}".ToLower();
+string url = $"https://smartword2vec.azurewebsites.net/api/Top3Similar?positive1={ord[1]}&positive2={ord[2]}&negative={ord[0]}".ToLower();
 HttpClient client = new ();   
 HttpResponseMessage response = await client.GetAsync(url);
 string responseBody = await response.Content.ReadAsStringAsync();
@@ -19,7 +20,6 @@ string[] resultater = responseBody.Split(';');
 //    a is to b, as c is to d
 //    (det mest sandsynlige ord er først i array'et)
 
+
 // 2) En udskrift af alle de ord, der kom med tilbage som resultater
-
-
 
