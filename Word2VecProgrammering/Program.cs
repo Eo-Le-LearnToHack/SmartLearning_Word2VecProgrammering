b68d750733aa953d9df4bab5ce54f5a6944ef7b2
Dan Nguyen <Dan.Nguyen@Rsyd.Dk>
2022-04-28 13:36:51 +0200

    Add solution to Word2VedProgrammering.

--- Word2VecProgrammering/Program.cs
+++ Word2VecProgrammering/Program.cs
@@ -1,10 +1,19 @@
 ﻿using System.Net;
+start:
+Console.Clear();
 
 string[] ord = new string[3];
 
 // Skriv koden her, som giver brugeren mulighed for at indtaste 3 ord,
 // som efterfølgende bliver gemt i array'et ord
-
+Console.WriteLine("Indtast 3 ord:");
+for (int indeks = 0; indeks < ord.Length; indeks++)
+{
+    ord[indeks] = Console.ReadLine();
+}
+Console.WriteLine("");
+Console.WriteLine("Please wait. Calling on service");
+Console.WriteLine("");
 // Det er er min kode, som du ikke behøver at rette i
 string url = $"https://smartword2vec.azurewebsites.net/api/Top3Similar?positive1={ord[1]}&positive2={ord[2]}&negative={ord[0]}".ToLower();
 HttpClient client = new ();   
@@ -23,7 +32,14 @@ string[] resultater = responseBody.Split(';');
 // 1) En udskrift af det mest sandsynlige svar på formen
 //    a is to b, as c is to d
 //    (det mest sandsynlige ord er først i array'et)
-
+Console.WriteLine($"{ord[0]} is to {ord[1]}, as {ord[2]} is to {resultater[0]}");
 
 // 2) En udskrift af alle de ord, der kom med tilbage som resultater
-
+foreach (string resultaterVærdi in resultater)
+{
+    Console.WriteLine(resultaterVærdi);
+}
+Console.WriteLine("");
+Console.WriteLine("Tryk på ENTER for at prøve igen.");
+Console.ReadLine();
+goto start;
